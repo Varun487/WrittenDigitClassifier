0bafdcbf8a8e085f2d3b15d22f8c6ab185eb06df
Varun487 <seshuvarun@gmail.com>
2021-03-21 19:16:25 +0530

    Modifided deployment script

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -1,13 +1,13 @@
 echo "Starting new deployment..."
 git checkout --orphan gh-pages
 echo "Building..."
-npm run build
-git --work-tree dist add --all
-echo "Commiting new changes..."
+npm run  --prefix UI/digit-classifier-ui/ build
+git --work-tree UI/digit-classifier-ui/dist add --all
+echo "Commit new changes..."
 git --work-tree dist commit -m 'Deploy'
 echo "Pushing to gh-pages..."
 git push origin HEAD:gh-pages --force
-rm -r dist
+rm -r UI/digit-classifier-ui/dist
 git checkout -f main
 git branch -D gh-pages
 echo "Successfully deployed!"
